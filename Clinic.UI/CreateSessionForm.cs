4093062cc5da1ffd874df25fffa0fb50ff833520
Igoptt <rodrigo.severim.10@gmail.com>
2021-09-05 22:40:41 +0100

    Client can make sessions with the required checks

--- Clinic.UI/CreateSessionForm.cs
+++ Clinic.UI/CreateSessionForm.cs
@@ -22,7 +22,7 @@ namespace Clinic.UI
             {
                 cb_ChooseTherapist.Items.Add($"{therapist.FirstName} {therapist.LastName}");
             }
-            cb_sessionHours.Items.AddRange(new []{"9:00", "10:00", "11:00"});
+            cb_sessionHours.Items.AddRange(new []{"9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00"});
         }
 
         private void btn_CreateSession_Click(object sender, EventArgs e)
